46cd2bfd39bc5c9eb4142ce67fab6fb2dd1048aa
dadaa <dadaaism@gmail.com>
2016-04-21 00:16:30 +0900

    change data structure

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+/*
 function execute(property, from, to) {
   if (typeof property === 'undefined') {
     console.error("property is undefined");
@@ -34,7 +35,7 @@ function execute(property, from, to) {
   document.body.removeChild(target);
   return { from: fromResult, half: halfResult, to: toResult };
 }
-
+*/
 function propertyToIDL(property) {
   if (property == 'float') {
     return 'cssFloat';
@@ -62,17 +63,23 @@ function appendElement(tag, classes, content, parent) {
   return element
 }
 
-function toUI(testcase, result) {
+function toUI(propertyName, testcase, result) {
   var resultsElement = document.getElementById("results");
   var resultElement =
     appendElement("div", ["result"], "", resultsElement);
 
-  var originalProperty = testcase.property + ":" +
-                         "[" + testcase.from + ", " + testcase.to + "]";
-  var animatableClass = testcase.isAnimatableProperty
-                        ? "animatable" : "no-animatable";
-  appendElement("div", ["cell", "property", animatableClass],
-                originalProperty, resultElement);
+  var originalProperty = propertyName + ":" + JSON.stringify(testcase.values);
+  var originalPropertyElement =
+    appendElement("div", ["cell", "property"], originalProperty, resultElement);
+  if (testcase.animatablePropertyName) {
+    originalPropertyElement.classList.add("animatable");
+    var animatablePropertyNameElement =
+      appendElement("a", [],
+                    testcase.animatablePropertyName, originalPropertyElement);
+    var specLink = CSSDataTypes[testcase.animatablePropertyName].link;
+    animatablePropertyNameElement.setAttribute("href", specLink);
+  }
+
   if (result.error) {
     var errorElement = appendElement("div", ["cell", "error"],
                                      result.error, resultElement);
@@ -92,6 +99,72 @@ function toUI(testcase, result) {
   }
 }
 
+function pushTestcase(testcases, datatypeOrTestcases,
+                      animatableListBySpec, animatablePropertyName) {
+  datatypeOrTestcases.forEach(function(datatypeOrTestcase) {
+    if (Array.isArray(datatypeOrTestcase)) {
+      testcases.push({ "animatablePropertyName": animatablePropertyName,
+                       "values": datatypeOrTestcase });
+    } else {
+      var variable = CSSDataTypes[datatypeOrTestcase];
+      if (!variable) {
+        console.error(datatypeOrTestcase + " is not found.");
+      } else {
+        if (animatableListBySpec.includes(datatypeOrTestcase)) {
+          animatablePropertyName = datatypeOrTestcase;
+        }
+        pushTestcase(testcases, variable.testcases,
+                     animatableListBySpec, animatablePropertyName);
+      }
+    }
+  });
+}
+
+function getTestcases(datatypes, animatableListBySpec) {
+  var testcases = [];
+  pushTestcase(testcases, datatypes, animatableListBySpec, null);
+  return testcases;
+}
+
+function animate(propertyName, values) {
+  var target = document.createElement("div");
+  target.id = "target";
+  document.body.appendChild(target);
+  var keyframe = {};
+  keyframe[propertyToIDL(propertyName)] = values;
+  var animation;
+  try {
+    animation = target.animate(keyframe,
+                               { duration: 1000, fill: "both" });
+  } catch (e) {
+    return { error: e.name + ":" + e.message};
+  }
+  var fromResult = getComputedStyle(target)[propertyName];
+  animation.currentTime = 500;
+  var halfResult = getComputedStyle(target)[propertyName];
+  animation.currentTime = 1000;
+  var toResult = getComputedStyle(target)[propertyName];
+
+  document.body.removeChild(target);
+  return { from: fromResult, half: halfResult, to: toResult };
+}
+
+function execute(propertyName, propertyData) {
+  var testcases = getTestcases(propertyData["test-datatypes"],
+                               propertyData["animatables"]);
+  testcases.forEach(function(testcase) {
+    var result = animate(propertyName, testcase.values);
+    toUI(propertyName, testcase, result);
+  });
+}
+
+function build() {
+  Object.keys(CSSProperties).forEach(function (propertyName) {
+    var result = execute(propertyName, CSSProperties[propertyName]);
+    //toUI(testcase, result);
+  });
+}
+
 document.addEventListener("DOMContentLoaded", function() {
   var resultsElement = document.getElementById("results");
   var resultElement =
@@ -103,8 +176,5 @@ document.addEventListener("DOMContentLoaded", function() {
   appendElement("div", ["cell", "value"], "50%", resultElement);
   appendElement("div", ["cell", "value"], "100%", resultElement);
 
-  TESTCASES.forEach(function(testcase) {
-    var result = execute(testcase.property, testcase.from, testcase.to);
-    toUI(testcase, result);
-  });
+  build();
 });
